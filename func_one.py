cd40002f7ce8152db8f7f0ce08735ae999d6c069
weibaichuan <weibaichuan5060@126.com>
2017-08-27 10:36:44 +0800

    mend the func_one

--- func_one.py
+++ func_one.py
@@ -1,12 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy
+import scipy
+import pandas
+
 
 def show_name(name):
-	print('name: '+name);
+    print('name: ' + name);
 
 
 def show_course(courses):
-	for name in cousers:
-		print(name);
+    for name in cousers:
+        print(name);
 
 
-number = list(range(3))
-print(number)
+number = [1, 4, 9, 16, 25]
+plt.plot(number)
+plt.show()
